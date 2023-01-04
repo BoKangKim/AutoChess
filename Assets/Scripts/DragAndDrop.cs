f6d280a9195b59346c74a13396e8f767069f0d36
chang-soo-kim <kimchangsoo225@gmail.com>
2023-01-04 16:12:51 +0900

    add Photon

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -6,39 +6,52 @@ public class DragAndDrop : MonoBehaviour
 {
     GameObject selectedObject;
     Camera cam;
+    int unitLayer;
+    int spaceLayer;
+    Vector3 beforePoint;
+
     private void Start()
     {
         cam = Camera.main;
+        spaceLayer = 1 << LayerMask.NameToLayer("Space");
+        unitLayer = 1 << LayerMask.NameToLayer("Unit");
+
     }
     private void Update()
     {
+
+
+        Debug.Log(CastRay(spaceLayer).collider);
+
 
         if (Input.GetMouseButtonDown(0))
         {
             if(selectedObject == null)
             {
-                RaycastHit hit = CastRay();
-
-                if (hit.collider != null)
+                CastRay(unitLayer);
+                if (CastRay(unitLayer).collider != null)
                 {
-                    if (!hit.collider.CompareTag("drag"))
-                    {
-                        return;
-                    }
-                    selectedObject = hit.collider.gameObject;
-                   
+                    selectedObject = CastRay(unitLayer).collider.gameObject;
+                    beforePoint = CastRay(spaceLayer).collider.gameObject.transform.position;
                 }
             }
+            //Drop
             else
             {
+                if (CastRay(spaceLayer).collider != null)
+                {
+                    Vector3 worldPosition = CastRay(spaceLayer).collider.gameObject.transform.position;
+                    selectedObject.transform.position = new Vector3(worldPosition.x, 0.5f, worldPosition.z);
 
-                Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.WorldToScreenPoint(selectedObject.transform.position).z);
-                //Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+                    selectedObject = null;
+                }
+                else
+                {
+                    selectedObject.transform.position = new Vector3(beforePoint.x , 0.5f, beforePoint.z) ;
+                    selectedObject = null;
 
-                Vector3 worldPosition = cam.ScreenToWorldPoint(position);
-                selectedObject.transform.position = new Vector3(worldPosition.x, 0, worldPosition.z);
-
-                selectedObject = null;
+                }
+               
             }
         }
         if (selectedObject != null)
@@ -47,19 +60,24 @@ public class DragAndDrop : MonoBehaviour
             Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.WorldToScreenPoint(selectedObject.transform.position).z);
             Vector3 worldPosition = cam.ScreenToWorldPoint(position);
 
-            selectedObject.transform.position = new Vector3(worldPosition.x, .25f, worldPosition.z);
+            selectedObject.transform.position = new Vector3(worldPosition.x, 1f, worldPosition.z);
         }
     }
 
-    RaycastHit CastRay()
+
+    RaycastHit CastRay(int Layer)
     {
         Vector3 screenMousePosFar = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.farClipPlane));
         Vector3 screenMousePosNear = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane));
-        RaycastHit hit;
-        Physics.Raycast(screenMousePosNear, screenMousePosFar - screenMousePosNear, out hit);
+
+        Physics.Raycast(screenMousePosNear, screenMousePosFar - screenMousePosNear, out RaycastHit hit, Vector3.Magnitude(screenMousePosFar - screenMousePosNear), Layer);
         Debug.DrawRay(screenMousePosNear, screenMousePosFar - screenMousePosNear, Color.red);
+
         return hit;
 
     }
-    
+
+
+
+
 }
