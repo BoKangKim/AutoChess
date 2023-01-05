a61f2eb683a01b894e8b578e744a290f4b6d9da3
Won-Hyuk-Lee <lwhprt@naver.com>
2023-01-05 17:44:11 +0900

    addScriptableObject

--- Assets/Scrpts/ClassSystem/ApplyUnitStats.cs
+++ Assets/Scrpts/ClassSystem/ApplyUnitStats.cs
@@ -10,28 +10,32 @@ namespace UnitStats
         public static float attackRange;
         public static float abilityPower; //주문력-> 롤에서 말하는 ap가 줄임말임
         public static float attackSpeed;
+        public static float grade;
 
-        public static void GetUnitStats()
+        public static void GetUnitStats() // csv or scriptableObject에서 데이터 받아올때
         {
 
         }
-        public static void GetUnitDate()
+
+        public static void GetUnitItem() // 장착 아이템 파악
         {
 
         }
 
-        public static void SetAllUnitStats()
+        public static void GetSynergy() // 적용 시너지 파악
         {
 
         }
+
+        public static void SetAllUnitStats() // 총 기물 갯수
+        {
+
+        }
+
+
     }
 
-    public static class SetSenergy
-    {
-        public static float test;
-        public static string test2;
-
-    }
+    
 
 
 }
