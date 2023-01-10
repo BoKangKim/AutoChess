d79dbb5ee18c028317f1860601c09ee213cc3756
sora <cpflskfn@gmail.com>
2023-01-10 20:25:52 +0900

    test DropItem

--- Assets/KSR/Script/DropItem.cs
+++ Assets/KSR/Script/DropItem.cs
@@ -1,13 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DropItem : MonoBehaviour
 {
-    // 생성 될 때 한 번 튕겨져서 위로 점프하듯이 나감
-    // 
-    float speed;
-    float jumped;
+    [SerializeField] private DropItem myPrefab;
+    
+    Vector3 forcePower;    
 
-
+    Rigidbody myRb;    
+    private void Start()
+    {       
+        myRb = GetComponent<Rigidbody>();
+        forcePower = new Vector3(Random.Range(-1, 2), 5, Random.Range(-1, 2));
+    }    
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Bounce();                      
+        }
+        if (transform.position.y >=6f)
+        {
+            Drop();            
+        }        
+        if (Input.GetMouseButtonDown(1)) 
+        {
+            SceneManager.LoadScene("Balltest");
+        }
+    }
+    private void Bounce()
+    {
+        myPrefab.myRb.useGravity = false;
+        myPrefab.myRb.AddForce(forcePower, ForceMode.Impulse);
+    }
+    private void Drop()
+    {        
+        myPrefab.myRb.useGravity = true;        
+    }
+    
+    
 }
