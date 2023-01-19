47f0f0ba72912ea14c1611391aa3813729f52bba
Won-Hyuk-Lee <lwhprt@naver.com>
2023-01-19 17:33:58 +0900

    add synergyTextUi

--- Assets/Scripts/DragDropSystem/MapController.cs
+++ Assets/Scripts/DragDropSystem/MapController.cs
@@ -57,6 +57,7 @@ namespace ZoneSystem
 
         public int BattleZoneCheck() //배틀존 모든 드롭 시점관여
         {
+            //Debug.Log("AA");
             unitCount = new Dictionary<string, int>();
             orcSynergyCount = 0;
             dwarfSynergyCount = 0;
@@ -130,27 +131,29 @@ namespace ZoneSystem
                             }
                             //이안에서 시너지 계산 다끝내고 
                             //종족(species)은 3,5 직업(class)은 2,4마리에서 각각 1,2단계가 활성화됨
+
+                            //이거 무조건 고쳐야함 뇌뽑고 구현만했는데 말안됨
                             if (warriorSynergyCount == 2)
                             {
                                 activeSynergyList.Add("Warrior");
-                                Debug.Log("1단계 워리어 달성");
+                                //Debug.Log("1단계 워리어 달성");
                             }
                             if (warriorSynergyCount == 4)
                             {
                                 activeSynergyList.Remove("Warrior");
                                 activeSynergyList.Add("Warrior2");
-                                Debug.Log("2단계 워리어 달성");
+                                //Debug.Log("2단계 워리어 달성");
                             }
 
                             if (tankerSynergyCount == 2) activeSynergyList.Add("Tanker");
-                            if (warriorSynergyCount == 4)
+                            if (tankerSynergyCount == 4)
                             {
                                 activeSynergyList.Remove("Tanker");
                                 activeSynergyList.Add("Tanker2");
                             }
 
                             if (assassinSynergyCount == 2) activeSynergyList.Add("Assassin");
-                            if(assassinSynergyCount == 4)
+                            if (assassinSynergyCount == 4)
                             {
                                 activeSynergyList.Remove("Assassin");
                                 activeSynergyList.Add("Assassin2");
@@ -170,44 +173,41 @@ namespace ZoneSystem
                                 activeSynergyList.Add("RangeDealer2");
                             }
 
-                            if (dwarfSynergyCount == 2) activeSynergyList.Add("Dwarf");
-                            if (dwarfSynergyCount == 4)
+                            if (dwarfSynergyCount == 3) activeSynergyList.Add("Dwarf");
+                            if (dwarfSynergyCount == 5)
                             {
                                 activeSynergyList.Remove("Dwarf");
                                 activeSynergyList.Add("Dwarf2");
                             }
 
-                            if (orcSynergyCount == 2) activeSynergyList.Add("Orc");
-                            if (orcSynergyCount == 4)
+                            if (orcSynergyCount == 3) activeSynergyList.Add("Orc");
+                            if (orcSynergyCount == 5)
                             {
                                 activeSynergyList.Remove("Orc");
                                 activeSynergyList.Add("Orc2");
                             }
 
-                            if (mechaSynergyCount == 2) activeSynergyList.Add("Mecha");
-                            if (mechaSynergyCount == 4)
+                            if (mechaSynergyCount == 3) activeSynergyList.Add("Mecha");
+                            if (mechaSynergyCount == 5)
                             {
                                 activeSynergyList.Remove("Mecha");
                                 activeSynergyList.Add("Mecha2");
                             }
 
-                            if (scorpionSynergyCount == 2) activeSynergyList.Add("Scorpion");
-                            if (scorpionSynergyCount == 4)
+                            if (scorpionSynergyCount == 3) activeSynergyList.Add("Scorpion");
+                            if (scorpionSynergyCount == 5)
                             {
                                 activeSynergyList.Remove("Scorpion");
                                 activeSynergyList.Add("Scorpion2");
                             }
 
-                            if (undeadSynergyCount == 2) activeSynergyList.Add("Undead");
-                            if (undeadSynergyCount == 4)
+                            if (undeadSynergyCount == 3) activeSynergyList.Add("Undead");
+                            if (undeadSynergyCount == 5)
                             {
                                 activeSynergyList.Remove("Undead");
                                 activeSynergyList.Add("Undead2");
                             }
-
-
-
-
+                            //Debug.Log(assassinSynergyCount);
                         }
                         synergyList.Clear();
                     }
@@ -215,10 +215,14 @@ namespace ZoneSystem
             }
 
             //여기서 시너지를 뱉어줘야함 근데 실제 유닛 적용은 계속 하는게 아니라 특정 지점에만 해줘(라운드 시작 직전)
-            for(int i = 0; i < activeSynergyList.Count; i++)
+            Debug.Log("액티브 시너지 리스트 카운트"+activeSynergyList.Count);
+            for(int i = 0; i<activeSynergyList.Count;i++)
             {
-                Debug.Log(activeSynergyList[i]);
+                Debug.Log("현재 활성화 된 시너지 " + activeSynergyList[i]);
             }
+            
+            UIManager.Inst.SynergyText(null);
+            //activeSynergyList.ForEach(str => UIManager.Inst.SynergyText(str));
 
             activeSynergyList.Clear();
             return battleUnitCount;
@@ -226,7 +230,6 @@ namespace ZoneSystem
 
         public void OnClick_UnitInst()
         {
-
             for (int z = 0; z < 2; z++)
             {
                 for (int x = 0; x < 7; x++)
