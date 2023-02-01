83653092bac0621169f0996c07f20b807204aa2b
bkkim <bkbk0329@naver.com>
2023-02-01 15:27:36 +0900

    Add Demon Range And Magician

---
+++ Assets/Scripts/AI/Unit/Demon/DemonMagicianAI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemonMagicianAI : RangeAI
+{
+    public override void StartSkillEffect()
+    {
+        if (myAni.GetParameter(2).name.CompareTo("activeSkill") == 0)
+        {
+            myAni.SetTrigger("activeSkill");
+        }
+
+        SkillEffect skill = null;
+        Instantiate(skillEffect.gameObject, target.transform.position + Vector3.up, Quaternion.identity).TryGetComponent<SkillEffect>(out skill);
+        skill.setOwnerName(nickName);
+    }
+}
