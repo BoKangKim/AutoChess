c36e9e90c2f554c89e83eafd1db441fe941cb864
bkkim <bkbk0329@naver.com>
2023-02-01 12:44:02 +0900

    Add Mecha Effects

---
+++ Assets/Scripts/AI/Unit/Mecha/MechaTankerAI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MechaTankerAI : MeleeAI
+{
+    public override void StartSkillEffect()
+    {
+        SkillEffect skill = null;
+        Vector3 targetPos = target.transform.position;
+        Instantiate(skillEffect.gameObject, new Vector3(transform.position.x, 1f, transform.position.z), Quaternion.identity).TryGetComponent<SkillEffect>(out skill);
+        skill.setOwnerName(nickName);
+    }
+}
