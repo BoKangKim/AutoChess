99a33a14ac8555b6cecc14afb12dd81ac67b5937
KSR <cpflskfn@gmail.com>
2023-02-01 17:23:21 +0900

    fixing UI

--- Assets/Scripts/UI/TimeManager.cs
+++ Assets/Scripts/UI/TimeManager.cs
@@ -8,29 +8,48 @@ public class TimeManager : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI Timer;
     [SerializeField] Image TimerIcon;
-    protected float maximumTime = 30;
-    protected float currentTime;
-    protected bool IsTimeOver { get; set; }
+    protected float maximumTime = 1;
+    public float currentTime;
+    public bool IsTimeOver { get; set; }
+    public bool IsNextRound { get; set; }
 
     private void Start()
     {
         IsTimeOver = false;
+        IsNextRound = false;
 
         currentTime = maximumTime;
         TimerIcon.fillAmount = maximumTime;
+
     }
     private void Update()
     {
         // Game Timer
         //photonView.RPC(nameof(TimeFlow), RpcTarget.AllBuffered); // nameof 참조중인지 확인하기 위함
-        if (IsTimeOver == true) return;
-        currentTime -= 1 * Time.deltaTime;
-        TimerIcon.fillAmount = (currentTime / maximumTime);
-        if (currentTime <= 0)
-        {
-            IsTimeOver = true;
+        if (IsTimeOver)
+        {            
             currentTime = 0;
         }
+        else 
+        {
+            currentTime -= 1 * Time.deltaTime;
+            if (currentTime <= 0)
+            {
+                currentTime = 0;
+                if (IsNextRound)
+                {
+                    // 경험치 골드 지급 시점
+                    // user.GetExp += 4;
+                    // user.Getgold += 10;
+                    currentTime = maximumTime;
+                    IsNextRound = false;
+                }
+            }
+        }
+        TimerIcon.fillAmount = (currentTime / maximumTime);
         Timer.text = Mathf.Floor(currentTime).ToString();
+        
     }
+    
+   
 }
--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -9,8 +9,28 @@ using TMPro;
 
 public class UIManager : MonoBehaviour
 {
+    #region Singleton
+    static public UIManager instance;
+    static public UIManager INSTANCE
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<UIManager>();
+                if (instance == null)
+                {
+                    instance = new GameObject("UIManager").AddComponent<UIManager>();
+                }
 
-    ScriptableUnit unitData;
+            }
+            return instance;
+        }
+    }
+    #endregion
+
+    //ScriptableUnit unitData;
+    Player_test player;
 
     [SerializeField] TextMeshProUGUI userID;
     [SerializeField] string playerID;
@@ -21,36 +41,55 @@ public class UIManager : MonoBehaviour
     [SerializeField] GameObject rankingContents;
 
     [SerializeField] TextMeshProUGUI RoundInfoNum;
-    [SerializeField] TextMeshProUGUI RoundStepNum;
-    [SerializeField] TextMeshProUGUI expansionGold;
-    [SerializeField] TextMeshProUGUI userGold;
+    [SerializeField] TextMeshProUGUI RoundStageNum;
+    [SerializeField] TextMeshProUGUI RoundDetailStepNum;
+    [SerializeField] TextMeshProUGUI RoundDetailStepNum1;
+    [SerializeField] TextMeshProUGUI RoundDetailStepNum2;
+    [SerializeField] TextMeshProUGUI RoundDetailStepNum3;
+    [SerializeField] TextMeshProUGUI RoundDetailStepNum4;
+    [SerializeField] TextMeshProUGUI expansionGold; // 확장 시 사용하는 골드
+    [SerializeField] TextMeshProUGUI playerLV; 
+    [SerializeField] TextMeshProUGUI playerGold; // 플레이어가 소지한 골드
+    [SerializeField] TextMeshProUGUI playerHP; 
     [SerializeField] TextMeshProUGUI gachaWeponGold;
     [SerializeField] TextMeshProUGUI gachaUnitGold;
+    [SerializeField] Slider expansionEXPSlider; //
+    [SerializeField] Slider playerHPSlider; //
 
-    protected int gold;
+    protected int playerGoldValue;
+    protected int playerLevelValue;
+    protected float playerEXPValue;
+    protected float playerHPValue;
+    protected int playerMaxHPValue;
+
+    protected int deployedUnit; // 배치된 유닛
     protected int gachaWeponGoldValue;
     protected int gachaUnitGoldValue;
     protected int expansionGoldValue;
-    protected int expansionLV;
-    protected int deployedUnit;
-    protected int roundStepNumber;
-    protected int roundNumber;
+    protected int expansionMaxEXPValue;
+
+    protected int roundStepNumber1 = 1;
+    protected int roundStepNumber2 = 1;
+    protected int roundTextColor;
+    protected int RoundRewardGold = 10;
+    protected float RoundRewardEXP = 4f;
     protected bool IsESC { get; set; }
     protected bool IsSynergy { get; set; }
     protected bool IsRanking { get; set; }
+    //protected bool IsCurRound { get; set; }
 
-
+    TimeManager timeManager;
     protected void Awake()
     {
         DontDestroyOnLoad(this);
     }
     protected void Start()
     {
-        unitData = FindObjectOfType<ScriptableUnit>();
+        //unitData = FindObjectOfType<ScriptableUnit>();
+        player = FindObjectOfType<Player_test>();
         //userID.text = unitData.GetUnitName; // user data
         userID.text = playerID;
         playerID = userID.text; // save user id
-        gold = 100; // user data
 
         // 고정 지불 비용
         expansionGoldValue = 10;
@@ -63,34 +102,117 @@ public class UIManager : MonoBehaviour
         IsESC = false;
         IsSynergy = false;
         IsRanking = false;
+        //IsCurRound = true;
 
         // data 받아올 것
-        expansionLV = 1;
-        deployedUnit = 3;
-        roundStepNumber = 3;
-        roundNumber = 1;
+        playerGoldValue = 1000;//player.GetGold;
+        playerLevelValue = 1;//player.GetLevel;
+        playerEXPValue = 0;//player.GetEXP;
+        playerLV.text = playerLevelValue.ToString(); // 레벨 텍스트
+        expansionEXPSlider.value = playerEXPValue; // 슬라이더
+        
+        expansionMaxEXPValue = 32; // 임시 - 데이터 받아오기
+        playerMaxHPValue = 1; 
 
+        deployedUnit = 3; // 임시
+        RoundStageNum.text = (roundStepNumber1 + "-" + roundStepNumber2).ToString();
+        timeManager = FindObjectOfType<TimeManager>();
 
+        RoundDetailInfo();
     }
+    
+
     protected void Update()
     {
-        if (gold <= 0) gold = 0;
-        userGold.text = (gold + " G").ToString();
+        if (playerGoldValue <= 0) playerGoldValue = 0;
+        playerGold.text = (playerGoldValue + " G").ToString();
+        // UpdatePlayerInfo();
 
         // Game Setting
         if (Input.GetKeyDown(KeyCode.Escape)) SettingMenuESC();
 
         SynergyContents();
         RankingContents();
-        Debug.Log("local Position X : " + synergyContents.transform.localPosition.x);
-        Debug.Log("Position X : " + synergyContents.transform.position.x);
-
+       
         // Round Info
-        RoundInfoNum.text = (expansionLV + " / " + deployedUnit).ToString();
-        RoundStepNum.text = (roundNumber + "-" + roundStepNumber).ToString();
+        RoundInfoNum.text = (playerLevelValue + " / " + deployedUnit).ToString();
+        // timer = 0 -> Next Round
+        if (timeManager.currentTime<=0f)
+        {
+            if ((roundStepNumber1 >= 9)&& (roundStepNumber2 >= 4))
+            {
+                roundStepNumber1 = 9;                
+                roundStepNumber2 = 4;
+                timeManager.IsTimeOver = true;
+                return;
+            } 
+
+            roundStepNumber2++;
+            if (roundStepNumber2 >= 5)
+            {
+                roundStepNumber1 += 1;
+                roundStepNumber2 = 1;
+            }
+            RoundStageNum.text = (roundStepNumber1 + "-" + roundStepNumber2).ToString();   
+            timeManager.IsNextRound = true;
+            // 한 라운드 끝나면 자동 1회 지급
+            // 조건
+            //OnFactionExpansion();
+        }
+        RoundDetailInfo();
+        playerHP.text = playerHPValue.ToString();
+        playerHPSlider.value = (playerHPValue * 0.1f);
+        playerHPSlider.value = (playerMaxHPValue * 0.1f);
+        // 다른 플레이어와 비교했을 때 가장 높은 HP일 경우 상위 노출
+        
     }
+   
+    // Round Detail Info
+    #region Round Detail Info
+    protected void RoundDetailInfo()
+    {        
+        RoundDetailStepNum.text = roundStepNumber1.ToString();
+        RoundDetailStepNum1.text = (roundStepNumber1 + "-" + 1).ToString();
+        RoundDetailStepNum2.text = (roundStepNumber1 + "-" + 2).ToString();
+        RoundDetailStepNum3.text = (roundStepNumber1 + "-" + 3).ToString();
+        RoundDetailStepNum4.text = (roundStepNumber1 + "-" + 4).ToString();
 
+        roundTextColor = 0;
+        if (roundStepNumber2 == 1) roundTextColor = 1;
+        else { ChangeRoundColorInitiate(RoundDetailStepNum1); }
+        
+        if (roundStepNumber2 == 2) roundTextColor = 2;
+        else { ChangeRoundColorInitiate(RoundDetailStepNum2); }
 
+        if (roundStepNumber2 == 3) roundTextColor = 3;
+        else { ChangeRoundColorInitiate(RoundDetailStepNum3); }
+
+        if (roundStepNumber2 == 4) roundTextColor = 4;
+        else { ChangeRoundColorInitiate(RoundDetailStepNum4); }
+
+        switch (roundTextColor) 
+        {
+            case 1:
+                ChangeRoundColor(RoundDetailStepNum1);
+                break;                
+            case 2:
+                ChangeRoundColor(RoundDetailStepNum2);
+                break;
+            case 3:
+                ChangeRoundColor(RoundDetailStepNum3);
+                break;
+            case 4:
+                ChangeRoundColor(RoundDetailStepNum4);
+                break;
+        }
+        Debug.Log("text color : " + RoundDetailStepNum1.color);
+    }
+    #endregion
+
+    // Text Color Change
+    protected void ChangeRoundColor(TextMeshProUGUI t) { t.color = Color.yellow; }
+    protected void ChangeRoundColorInitiate(TextMeshProUGUI t) { t.color = Color.gray; }
+  
     // Setting Menu
     #region Setting Menu
     public void OnSettingMenu()
@@ -210,24 +332,38 @@ public class UIManager : MonoBehaviour
     protected int MixRandomWepon;
     public void OnWeponGacha()
     {
-        if (gold <= gachaWeponGoldValue) return;
-        MixRandomWepon = Random.Range(0, 4);
-        gold -= gachaWeponGoldValue;
+        if (playerGoldValue <= gachaWeponGoldValue) return;        
+        playerGoldValue -= gachaWeponGoldValue;
     }
     // Gacha Unit UI
     protected int MixRandomUnit;
     public void OnUnitGacha()
     {        
-        if (gold <= gachaUnitGoldValue) return;
-        MixRandomUnit = Random.Range(0, 21); // 뽑을 때마다 확률 랜덤
-        gold -= gachaUnitGoldValue;
+        if (playerGoldValue <= gachaUnitGoldValue) return;
+        
+        playerGoldValue -= gachaUnitGoldValue;
     }
 
     // Faction Expansion UI
     public void OnFactionExpansion()
     {
-        if (gold <= expansionGoldValue) return;
-        gold -= expansionGoldValue;
+        if (playerGoldValue <= expansionGoldValue) return;
+        if (playerEXPValue >= expansionMaxEXPValue)
+        {
+            playerLevelValue += 1;
+            playerLV.text = playerLevelValue.ToString();
+            playerEXPValue = playerEXPValue - expansionMaxEXPValue;
+            // 
+        }        
+            playerGoldValue -= expansionGoldValue;
+            playerGoldValue += RoundRewardGold;
+            playerEXPValue += RoundRewardEXP;
+       
+        playerGold.text = playerGoldValue.ToString();
+        playerLV.text = playerLevelValue.ToString(); //
+        expansionEXPSlider.value = (playerEXPValue * 0.1f); // 슬라이더 값 조절 필요
+        expansionEXPSlider.maxValue = (expansionMaxEXPValue * 0.1f);
+        Debug.Log("playerEXPValue : " + playerEXPValue);        
     }
 
     protected void WeponRandomCalculation()
@@ -237,6 +373,7 @@ public class UIManager : MonoBehaviour
         // safety zone 에 배치
         // 예외처리 : 여유 공간 없음
         // 맵 스크립트 참고
+        // MixRandomUnit = Random.Range(0, 21); // 뽑을 때마다 확률 랜덤
     }
     protected void UnitRandomCalculation()
     {
@@ -246,93 +383,15 @@ public class UIManager : MonoBehaviour
         // 2성 오크 하나 판매 -> 1성 오크 2개 증가
         // gachaUnit/maxUnit
         
-        
-        int[] RadomUnitList;
-
-
-        // 해당 데이터에 맞는 번호의 유닛 제공
-        int GetRandomUnit = Random.Range(0, 22);
-        // 이미 뽑은 유닛 중복 X -> 뽑은 유닛 삭제
-
-        switch (GetRandomUnit)
-        {            
-            case 1:
-                Debug.Log("유닛 뽑기 - 1");
-                break;
-            case 2:
-                Debug.Log("유닛 뽑기 - 2");
-                break;
-            case 3:
-                Debug.Log("유닛 뽑기 - 3");
-                break;
-            case 4:
-                Debug.Log("유닛 뽑기 - 4");
-                break;
-            case 5:
-                Debug.Log("유닛 뽑기 - 5");
-                break;
-            case 6:
-                Debug.Log("유닛 뽑기 - 6");
-                break;
-            case 7:
-                Debug.Log("유닛 뽑기 - 7");
-                break;
-            case 8:
-                Debug.Log("유닛 뽑기 - 8");
-                break;
-            case 9:
-                Debug.Log("유닛 뽑기 - 9");
-                break;
-            case 10:
-                Debug.Log("유닛 뽑기 - 10");
-                break;
-            case 11:
-                Debug.Log("유닛 뽑기 - 11");
-                break;
-            case 12:
-                Debug.Log("유닛 뽑기 - 12");
-                break;
-            case 13:
-                Debug.Log("유닛 뽑기 - 13");
-                break;
-            case 14:
-                Debug.Log("유닛 뽑기 - 14");
-                break;
-            case 15:
-                Debug.Log("유닛 뽑기 - 15");
-                break;
-            case 16:
-                Debug.Log("유닛 뽑기 - 16");
-                break;
-            case 17:
-                Debug.Log("유닛 뽑기 - 17");
-                break;
-            case 18:
-                Debug.Log("유닛 뽑기 - 18");
-                break;
-            case 19:
-                Debug.Log("유닛 뽑기 - 19");
-                break;
-            case 20:
-                Debug.Log("유닛 뽑기 - 20");
-                break;
-            case 21:
-                Debug.Log("유닛 뽑기 - 21");
-
-                break;
-        }
+        // MapController 에서 데이터 참고
     }
-
+    // 라운드 넘어가는 판단 먼저 진행 -> exp 계산 
+    // 타이머 0일때 다음 라운드 진행
     // 진영 Exp 가 max 면 진영 level up
-    protected Slider FactionExp;
-    protected int FactionExpValue;
-    protected TextMeshProUGUI FactionLeveltext;
-    protected int FactionLevelValue;
-    protected void FactionLevel()
-    {
-        FactionExp.value = FactionExpValue;
-    }
-
+    // 한 라운드 당 4씩 오름
+    // curexp/maxexp - exp 차액만큼 골드 지불
+    // 4/12 - 8골드 소비 - exp 4 증가
+   
     // Game Quit
     public void QuitGame()
     {
